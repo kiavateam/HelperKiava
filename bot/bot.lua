400b1f1e69e33f4c4675abe4f2c3e96662d2bac9
kiavateam <ava@kiava.ir>
2017-06-02 01:59:35 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,6 +1,6 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-    bot_token = "290199016:AAGpsprXVaE0xgrysKucrlMP1ej9ZqhNaVU" --Put You Token Here
+    bot_token = "361169019:AAE66ZsFat8Vv_nsfDlqKAhWNK0SUaLCOBA" --Put You Token Here
    send_api = "https://api.telegram.org/bot"..bot_token
 sudo_id = 247134702,392781647,366848429,334984718
 require('./bot/methods')
