3bd38569b76422f46996d137d093cef84aee287a
kiavateam <ava@kiava.ir>
2017-07-14 10:57:42 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,8 +1,8 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-    bot_token = "427527171:AAFZuOCaPTOjGTfmr2nQMZOziyvb2oqzrEw" --Put You Token Here
+    bot_token = "387388679:AAHrx9-ogqGY8p_7jJlZmKiU3LcCJutQHlk" --Put You Token Here
    send_api = "https://api.telegram.org/bot"..bot_token
-sudo_id = 247134702,439295927,366848429,334984718
+sudo_id = 247134702,271713872,366848429,334984718
 require('./bot/methods')
 require('./bot/utils')
 require('./libs/JSON')
@@ -162,7 +162,7 @@ function create_config( )
     "core",
     "plugins"
     },
-    sudo_users = {247134702,439295927,366848429,334984718},--Sudo users
+    sudo_users = {247134702,271713872,366848429,334984718},--Sudo users
     admins = {},
     disabled_channels = {},
     moderation = {data = ''..kiavair..'/data/moderation.json'},
